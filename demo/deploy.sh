abe054a9e98c8fa96b67a16f3bdc5af8adc959f6
Mike Lapshin <mikhail.a.lapshin@gmail.com>
2014-03-04 16:28:18 +0400

    deploy at Tue Mar  4 16:28:18 MSK 2014 by Mike Lapshin

--- demo/deploy.sh
+++ demo/deploy.sh
@@ -2,9 +2,24 @@
 
 # formstamp.github.io deploy script
 # run it from repository root
-
 set +e # break script on first error
-source ~/.nvm/nvm.sh
+
+# make sure script is run from correct place
+if [ ! -d .git ]; then
+    echo "ERROR: You should run this script from ROOT of formstamp GIT repo"
+    echo "Example: demo/deploy.sh"
+    exit 1
+fi
+
+# make sure formstamp.github.io repo exist
+if [ ! -d ../formstamp.github.io/.git ]; then
+    CWD=`pwd`
+    echo "ERROR: You should have formstamp.github.io repo as a sibling directory of formstamp main repo. Run following command:"
+    echo "cd `dirname $CWD` && git clone git@github.com:formstamp/formstamp.github.io.git"
+    exit 1
+fi
+
+source ~/.nvm/nvm.sh # make sure nvm command is available
 
 # install dependencies
 bower install
